eae20afd1e311a144864bf561ee18f0280d0ebbe
JavaProjects <jerimiahmpdo1234@gmail.com>
2023-11-11 19:19:09 -0800

    java main

---
+++ src/ProgrammingChallenges/NumberToMonth.java
@@ -0,0 +1,38 @@
+package ProgrammingChallenges;
+
+import java.util.Scanner;
+
+public class NumberToMonth {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        System.out.print("\nEnter the Number of Month: ");
+
+        int month = scanner.nextInt();
+
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                System.out.println("31 days");
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                System.out.println("30 days");
+                break;
+            case 2:
+                System.out.println("28 days");
+                break;
+            default:
+                System.out.println("Invalid month number. Please enter a number between 1 and 12.");
+                break;
+        }
+        scanner.close();
+
+    }
+}
