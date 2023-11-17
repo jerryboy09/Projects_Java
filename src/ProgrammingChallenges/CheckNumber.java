f6fe5b4f4b6110f46586f9f4bbed3746f70e4a46
JavaProjects <jerimiahmpdo1234@gmail.com>
2023-11-16 17:42:12 -0800

    java main

---
+++ src/ProgrammingChallenges/CheckNumber.java
@@ -0,0 +1,24 @@
+package ProgrammingChallenges;
+import java.util.Scanner;
+
+public class CheckNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (num > 0) {
+            System.out.println("The number is positive.");
+            if (num % 2 == 0) {
+                System.out.println("The number is even.");
+            } else {
+                System.out.println("The number is odd.");
+            }
+        } else if (num < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("Zero");
+
+        }
+    }
+}
